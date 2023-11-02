890445af38dbb5701e0acff31af7ce1c17be3ac1
Begoingto <begoingto.me@gmail.com>
2023-11-02 16:44:25 +0700

    change setup mount path to only one master mount

--- nsf/setup-export.sh
+++ nsf/setup-export.sh
@@ -4,3 +4,5 @@ sudo echo "/opt/nfs/data 10.148.0.2(rw,sync,no_subtree_check) \
 10.184.0.2(rw,sync,no_subtree_check) \
 10.184.0.3(rw,sync,no_subtree_check) \
 " >> /etc/exports
+
+sudo echo "/opt/nfs/data *(rw,sync,no_subtree_check) # All IPs" >> /etc/exports
