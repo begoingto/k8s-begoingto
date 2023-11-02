820bf09d7cd8ac55977bf7640cc7f7ec1bd3e9e4
Begoingto <begoingto.me@gmail.com>
2023-11-02 15:56:23 +0700

    add ansible setup nsf

---
+++ nsf/setup-export.sh
@@ -0,0 +1,6 @@
+sudo echo "/opt/nfs/data 10.148.0.2(rw,sync,no_subtree_check) \
+10.148.0.3(rw,sync,no_subtree_check) \
+10.148.0.4(rw,sync,no_subtree_check) \
+10.184.0.2(rw,sync,no_subtree_check) \
+10.184.0.3(rw,sync,no_subtree_check) \
+" >> /etc/exports
